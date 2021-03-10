46c8c1028f6848f55f2471b33627a8881dd6b52c
Gregory DEPUILLE <gregory.depuille@sogelink.fr>
2021-03-10 19:52:45 +0100

    Upgrade Firmware
    # Liste des mots clés de titre de commit (version courte -> résultat long)
    # r <num> -> Récupération du Redmine (Redmine #<num> : <titre_du_redmine>)
    # t <titre> -> Technique : <titre>
    # f <titre> -> Fix : <titre>
    # ty <titre> -> Typo : <titre>
    # d <titre> -> Doc : <titre>
    # s <titre> -> Sécurité : <titre>
    # u <titre> -> UI : <titre>
    # c <titre> -> CI : <titre>
    # WIP <desc>
    # [skip ci] en début de titre de commit permet de ne pas déclencher de build
    #
    # Chaque ligne de description ajouté sera préfixé automatiquement d'une puce.
    #
    # Pour la saisie de temps automatique lors de l'écriture d'un commit le format est le suivant :
    # /spend <options>
    #
    # NB : Plusieurs /spend peuvent être défini sur un même commit
    #
    # Les options sont :
    # - Temps en heure (ie 0.25). Obligatoire
    # - Numéro de redmine. Facultatif si le commit porte sur un redmine avec la syntaxe courte ou longue.
    # - Catégorie de l'imputation. Facultatif. Récupération par les premières lettres en format court, si non défini Développement sera utilisé
    # - Date de l'imputation au format ISO8601 (ie 2021-02-23)
    #
    # NB : L'imputation n'est pas bloquante pour le message de commit si une erreur survient.
    - CMSIS 2.3.4 -> 2.3.5
    - STM32Cube FW_F3xx 1.11.1 -> 1.11.2
    - STM32F3xx HAL Driver 1.5.3 -> 1.5.5
    - STM32Cube MX 6.0.1 -> 6.2.0

--- Src/main.c
+++ Src/main.c
@@ -732,7 +732,7 @@ void mainProcess(void const * argument)
   /* USER CODE END mainProcess */
 }
 
-/**
+ /**
   * @brief  Period elapsed callback in non blocking mode
   * @note   This function is called  when TIM1 interrupt took place, inside
   * HAL_TIM_IRQHandler(). It makes a direct call to HAL_IncTick() to increment
